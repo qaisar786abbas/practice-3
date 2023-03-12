22c557a26f630dc999328aefe827a0961c6669bb
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-03-12 21:08:26 +0500

    added files

---
+++ Practice 3/app.js
@@ -0,0 +1,118 @@
+// var headUser = prompt("Enter head username");
+// var tailsUser = prompt("Enter tails username");
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss);
+// if(floor == 0){
+//     alert("Head" , headUser , "win the toss.")
+// }
+// else{
+//     alert("Tails" , tailsUser , "win the toss.");
+// }
+
+
+
+// var passwordCriteria = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+// var password = " ";
+// var passwordLength = 10;
+// var index;
+// for(var i=0; i<passwordLength; i++){
+//    index = Math.floor(Math.random()*passwordCriteria.length); 
+//    console.log(index);
+//    console.log(passwordCriteria[index]);
+//    password += passwordCriteria[index];
+// }
+// console.log(password);
+
+
+// var str = "123";
+// alert(str);
+
+// var str = +"123";
+// alert(str);
+// str.Number();
+// alert(str);
+
+
+// var num = 123;
+// num.toString();
+// alert(num);
+
+// var pi = 22/7;
+// str = pi.toFixed(6);
+// alert(str);
+
+
+
+// var number = Math.random() * 6;
+// alert(number);
+
+
+
+// var num = Math.round(6.76);
+// alert(num);
+
+// var num = Math.ceil(6.76);
+// alert(num);
+
+
+// var num = Math.floor(6.76);
+// alert(num);
+
+
+
+// var str = "Hello, My name is qaisar abbas";
+// for(var i=0; i<str.length; i++){
+//     alert(str.charAt(i));
+// }
+
+
+// var str ="qaisar";
+// alert(str.indexOf("s"));
+
+
+// var str ="qaisar";
+// alert(str.lastIndexOf("s"));
+
+// var str = "qaisar";
+// var result = str[0].toUpperCase() + str.slice(1);
+// alert(result);
+
+
+// var str = "qaisar abbas ali";
+// var words = str.split(" ");
+// for(var i=0; i<words.length; i++){
+
+// alert(words.join(" "));
+//     var result = words[i][0].toUpperCase() + words[1].slice(1);
+//   }
+//     arr.push(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
